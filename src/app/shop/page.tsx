0a3d083025af1ec9d2b63b79ce0b087224213f3c
Hamzaiqbal0 <hamzaiqbal.9.9.1.1.1@gmail.com>
2024-12-13 14:55:54 +0500

    xyz

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -7,6 +7,8 @@ const ShopPage = () => {
   return (
     <div className="w-full">
       <Navbar />
+      
+      {/* Breadcrumb Section */}
       <section className="w-full h-[92px] bg-white mx-auto flex items-center justify-between p-4">
         <h2 className="text-2xl font-semibold ml-10">Shop</h2>
         <p className="text-gray-600 mr-10">Home &gt; <b>Shop</b></p>
@@ -14,11 +16,11 @@ const ShopPage = () => {
 
       {/* Image Gallery Section */}
       <section className="w-[1440px] h-[271px] mx-auto flex gap-4 p-4 ml-20">
-        <div className="w-[205px] h-[223px] bg-gray-300 flex items-center justify-center">Image 1</div>
-        <div className="w-[205px] h-[223px] bg-gray-300 flex items-center justify-center">Image 2</div>
-        <div className="w-[205px] h-[223px] bg-gray-300 flex items-center justify-center">Image 3</div>
-        <div className="w-[205px] h-[223px] bg-gray-300 flex items-center justify-center">Image 4</div>
-        <div className="w-[205px] h-[223px] bg-gray-300 flex items-center justify-center">Image 5</div>
+        <img src="/image1.jpg" alt="Image 1" className="w-[205px] h-[223px] object-cover" />
+        <img src="/image2.jpg" alt="Image 2" className="w-[205px] h-[223px] object-cover" />
+        <img src="/image3.jpg" alt="Image 3" className="w-[205px] h-[223px] object-cover" />
+        <img src="/image4.jpg" alt="Image 4" className="w-[205px] h-[223px] object-cover" />
+        <img src="/image5.jpg" alt="Image 5" className="w-[205px] h-[223px] object-cover" />
       </section>
 
       {/* Filter Section */}
@@ -44,22 +46,61 @@ const ShopPage = () => {
           <button className="p-2 w-20 bg-blue-500 text-white rounded mr-60">Filter</button>
         </div>
       </section>
-      <div>
-        <section className="w-[1440px] h-[175px] mx-auto p-4 ">
+
+      {/* Banner Image Section */}
+      <section className="w-[1440px] h-[175px] mx-auto p-4 mb-10">
         <img 
           src="/row.png" 
           alt="Banner" 
           className="w-[1054px] h-[175px] mx-auto object-cover ml-28"
         />
       </section>
-    </div>
+      <h2 className="font-semibold text-2xl text-center mb-12 text">Related Products</h2>
+
+      {/* New Section with 3 Subsections and Pagination */}
+      <section className="w-[1440px] h-[1778px] mx-auto p-4">
+        <div className="w-[1124px] h-[1778px] mx-auto">
+          {/* 3 Subsections with 4 Images Each */}
+          <div className="flex flex-col gap-16">
+            {/* Subsection 1 */}
+            <div className="flex gap-4">
+              <img src="/col-md-3 (1).png" alt="Image 1" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (2).png" alt="Image 2" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (3).png" alt="Image 3" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (4).png" alt="Image 4" className="w-[238px] h-[488px] object-cover" />
+            </div>
+
+            {/* Subsection 2 */}
+            <div className="flex gap-4">
+              <img src="/col-md-3 (5).png" alt="Image 5" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (6).png" alt="Image 6" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (7).png" alt="Image 7" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (8).png" alt="Image 8" className="w-[238px] h-[488px] object-cover" />
+            </div>
+
+            {/* Subsection 3 */}
+            <div className="flex gap-4">
+              <img src="/col-md-3 (9).png" alt="Image 9" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (10).png" alt="Image 10" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3 (11).png" alt="Image 11" className="w-[238px] h-[488px] object-cover" />
+              <img src="/col-md-3.png" alt="Image 12" className="w-[238px] h-[488px] object-cover" />
+            </div>
+          </div>
+
+          {/* Pagination Image */}
+          <div className="mt-16 flex justify-center">
+            <img 
+              src="/ul.png" 
+              alt="Pagination" 
+              className="w-[313px] h-[74px] object-cover"
+            />
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </div>
-    
-    
-    
   );
 };
-
 
 export default ShopPage;
